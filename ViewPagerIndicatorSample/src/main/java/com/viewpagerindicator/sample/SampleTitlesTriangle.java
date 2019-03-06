0af53fd50c5b377f703d9f396d46bf7f3441d5c6
coderliijin <1753139102@qq.com>
2019-03-06 08:57:35 +0800

    第二次提交

---
+++ ViewPagerIndicatorSample/src/main/java/com/viewpagerindicator/sample/SampleTitlesTriangle.java
@@ -0,0 +1,26 @@
+package com.viewpagerindicator.sample;
+
+import android.os.Bundle;
+
+import com.viewpagerindicator.TitlePageIndicator;
+import com.viewpagerindicator.TitlePageIndicator.IndicatorStyle;
+
+import androidx.viewpager.widget.ViewPager;
+
+public class SampleTitlesTriangle extends BaseSampleActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_titles);
+
+        mAdapter = new TestFragmentAdapter(getSupportFragmentManager());
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        TitlePageIndicator indicator = (TitlePageIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+        indicator.setFooterIndicatorStyle(IndicatorStyle.Triangle);
+        mIndicator = indicator;
+    }
+}
